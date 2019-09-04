3f4f59359cf850034aaa189e607b9be938dc001d
Meghan Stang <mstang15@gmail.com>
2019-09-04 09:49:30 -0600

    Add namespaced route

---
+++ app/controllers/admin/cats_controller.rb
@@ -0,0 +1,3 @@
+class Admin::CatsController <ApplicationController
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get '/cats', to: 'cats#index'
+  get '/cats/:id', to: 'cats#show'
+
+
+#admin functionality
+namespace :admin do
+  get '/cats', to: 'cats#index'
+  get '/cats/:id', to: 'cats#show'
+  get '/cats/:id/edit', to: 'cats#edit', as: :edit
+  patch '/cats/:id', to: 'cats#update'
+  delete '/cats/:id', to: 'cats#destroy'
+end
+
 end
